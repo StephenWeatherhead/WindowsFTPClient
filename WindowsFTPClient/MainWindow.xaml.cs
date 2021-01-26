1629c034ff59c142e7dcf64880e893e47ada1c55
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-26 22:07:11 +0000

    Connect to FTP server and list files in working directory

--- WindowsFTPClient/MainWindow.xaml.cs
+++ WindowsFTPClient/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,25 @@ namespace WindowsFTPClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ConnectTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            using (FtpClient client = new FtpClient(HostTextBox.Text))
+            {
+                client.Credentials = new NetworkCredential(UsernameTextBox.Text, PasswordTextBox.SecurePassword);
+                client.Port = int.Parse(PortTextBox.Text);
+                client.OnLogEvent += LogEvent;
+                client.Connect();
+                FilesGrid.ItemsSource = client.GetListing();
+                client.Disconnect();
+            }
+        }
+
+        private void LogEvent(FtpTraceLevel arg1, string arg2)
+        {
+            LogTextBox.AppendText(DateTime.Now + "\t" + arg1 + "\t" + arg2 + Environment.NewLine);
+            LogTextBox.ScrollToEnd();
         }
     }
 }
