c1967129a67d5892d197bff5da0c5534a26ef3aa
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-29 21:42:27 +0000

    starting to define ftp client interface

---
+++ WindowsFTPClient.ViewModels/IWFTPClient.cs
@@ -0,0 +1,20 @@
+﻿using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFTPClient.ViewModels
+{
+    public interface IWFTPClient : IDisposable
+    {
+        Task ConnectAsync(CancellationToken cancellationToken);
+        Task DisconnectAsync(CancellationToken cancellationToken);
+        Task<string> GetWorkingDirectoryAsync(CancellationToken cancellationToken);
+        Task SetWorkingDirectoryAsync(string workingDirectory, CancellationToken cancellationToken);
+        bool IsConnected { get; }
+        event EventHandler IsConnectedChanged;
+        event Action<FtpTraceLevel, string> Log;
+    }
+}
