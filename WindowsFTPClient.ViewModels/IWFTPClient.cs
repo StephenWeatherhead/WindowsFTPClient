3627e1c8a31bc52a1972088424e3402b52ec1feb
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-31 21:20:21 +0000

    FTP client interface

--- WindowsFTPClient.ViewModels/IWFTPClient.cs
+++ WindowsFTPClient.ViewModels/IWFTPClient.cs
@@ -15,12 +15,17 @@ namespace WindowsFTPClient.ViewModels
         Task<ServiceResult<string>> GetWorkingDirectoryAsync(CancellationToken cancellationToken);
         Task<ServiceResult> SetWorkingDirectoryAsync(string workingDirectory, CancellationToken cancellationToken);
         Task<ServiceResult<List<FileViewModel>>> GetListingAsync(CancellationToken cancellationToken);
-        Task<ServiceResult> DownloadAsync(Stream outStream, string remotePath, long restartPosition, Action<FtpProgress> progress, CancellationToken cancellationToken);
+        Task<ServiceResult<FtpResult>> DownloadFileAsync(string localPath, string remotePath, IProgress<FtpProgress> progress, CancellationToken cancellationToken);
+        Task<ServiceResult<List<FtpResult>>> DownloadDirectoryAsync(string localFolder, string remoteFolder, IProgress<FtpProgress> progress, CancellationToken cancellationToken);
         Task<ServiceResult> DeleteFileAsync(string path, CancellationToken cancellationtoken);
         Task<ServiceResult> CreateDirectoryAsync(string path, CancellationToken cancellationtoken);
         Task<ServiceResult> DeleteDirectoryAsync(string path, CancellationToken cancellationtoken);
-
-
+        Task<ServiceResult> MoveDirectoryAsync(string path, string dest, CancellationToken cancellationToken);
+        Task<ServiceResult> MoveFileAsync(string path, string dest, CancellationToken cancellationToken);
+        Task<ServiceResult<FtpStatus>> UploadFileAsync(string localPath, string remotePath, IProgress<FtpProgress> progress, CancellationToken cancellationToken);
+        Task<ServiceResult<int>> UploadFilesAsync(IEnumerable<string> localPaths, string remoteDir, CancellationToken  cancellationToken, IProgress<FtpProgress> progress);
+        Task<ServiceResult> SetFilePermissionsAsync(string path, FtpPermission owner, FtpPermission group, FtpPermission other, CancellationToken cancellationToken);
+        Task<ServiceResult<List<FtpResult>>> UploadDirectoryAsync(string localFolder, string remoteFolder, IProgress<FtpProgress> progress, CancellationToken cancellationToken);
         event Action<FtpTraceLevel, string> Log;
     }
 }
