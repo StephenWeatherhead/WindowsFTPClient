faaaffed38f92c6bb6d0ed03c66c3129e973df63
Stephen Weatherhead <stephenw42@outlook.com>
2021-02-02 21:32:54 +0000

    clear PropertyChanged when dispose called

--- WindowsFTPClient.ViewModels/ViewModel.cs
+++ WindowsFTPClient.ViewModels/ViewModel.cs
@@ -5,12 +5,18 @@ using System.Text;
 
 namespace WindowsFTPClient.ViewModels
 {
-    public class ViewModel : INotifyPropertyChanged
+    public class ViewModel : INotifyPropertyChanged, IDisposable
     {
         protected void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        public virtual void Dispose()
+        {
+            PropertyChanged = null;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
