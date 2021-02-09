d068f7edc35e0754bf8ad44dd470962306dabe0a
Stephen Weatherhead <stephenw42@outlook.com>
2021-02-09 20:49:10 +0000

    make FileViewModel observable

--- WindowsFTPClient.ViewModels/FileViewModel.cs
+++ WindowsFTPClient.ViewModels/FileViewModel.cs
@@ -5,17 +5,137 @@ using FluentFTP;
 
 namespace WindowsFTPClient.ViewModels
 {
-    public class FileViewModel
+    public class FileViewModel : ViewModel
     {
-        public string Name { get; set; }
-        public DateTime Modified { get; set; }
-        public FtpFileSystemObjectType Type { get; set; }
-        public long  Size { get; set; }
-        public string Permissions { get; set; }
-        public string OwnerPermissions { get; set; }
-        public string GroupPermissions { get; set; }
-        public DateTime Created { get; set; }
-        public string FullName { get; set; }
-        public bool IsSelected { get; set; }
+        private string _name;
+        public string Name 
+        { 
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+        private DateTime _modified;
+        public DateTime Modified 
+        { 
+            get
+            {
+                return _modified;
+            }
+            set
+            {
+                _modified = value;
+                RaisePropertyChanged(nameof(Modified));
+            }
+        }
+        private FtpFileSystemObjectType _type;
+        public FtpFileSystemObjectType Type 
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+                RaisePropertyChanged(nameof(Type));
+            }
+        }
+        private long _size;
+        public long  Size 
+        { 
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+                RaisePropertyChanged(nameof(Size));
+            }
+        }
+        private string _permissions;
+        public string Permissions 
+        { 
+            get
+            {
+                return _permissions;
+            }
+            set
+            {
+                _permissions = value;
+                RaisePropertyChanged(nameof(Permissions));
+            }
+        }
+        private string _ownerPermissions;
+        public string OwnerPermissions 
+        { 
+            get
+            {
+                return _ownerPermissions;
+            }
+            set
+            {
+                _ownerPermissions = value;
+                RaisePropertyChanged(nameof(OwnerPermissions));
+            }
+        }
+        private string _groupPermissions;
+        public string GroupPermissions 
+        { 
+            get
+            {
+                return _groupPermissions;
+            }
+            set
+            {
+                _groupPermissions = value;
+                RaisePropertyChanged(nameof(GroupPermissions));
+            }
+        }
+        private DateTime _created;
+        public DateTime Created 
+        {
+            get
+            {
+                return _created;
+            }
+            set
+            {
+                _created = value;
+                RaisePropertyChanged(nameof(Created));
+            }
+        }
+        private string _fullName;
+        public string FullName 
+        { 
+            get
+            {
+                return _fullName;
+            }
+            set
+            {
+                _fullName = value;
+                RaisePropertyChanged(nameof(FullName));
+            }
+        }
+        private bool _isSelected;
+        public bool IsSelected 
+        { 
+            get
+            {
+                return _isSelected;
+            }
+            set
+            {
+                _isSelected = value;
+                RaisePropertyChanged(nameof(IsSelected));
+            }
+        }
     }
 }
